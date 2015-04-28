5135f7c9c3ae0ebb5ac36746d927b58554882dee
Tommy Ingulfsen <tommyi@stanford.edu>
2015-04-28 15:25:03 -0700

    Updating with more tests.

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem "modsulator", :git => 'https://github.com/sul-dlss/modsulator'
 gem "rack-test", :require => "rack/test"
 
 group :test do
-  gem "equivalent-xml"
+  gem "equivalent-xml", '>= 0.6.0'   # For ignoring_attr_values() with arguments
   gem "rspec"
 end
 
--- spec/modsulator_app_spec.rb
+++ spec/modsulator_app_spec.rb
@@ -1,4 +1,6 @@
+require 'equivalent-xml'
 require 'spec_helper'
+
 
 describe Spreadsheet::ModsulatorAPI do
   def app
@@ -16,5 +18,19 @@ describe Spreadsheet::ModsulatorAPI do
     get '/v1/modsulator_version'
     expect(last_response).to be_ok
     expect(last_response.body).to match(/\d\.\d\.\d$/)
+  end
+
+  it "returns correct XML for a known spreadsheet" do
+    post "/v1/modsulator", "file" => Rack::Test::UploadedFile.new(File.join(FIXTURES_DIR, 'crowdsourcing_bridget_1.xlsx'), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    returned_xml = Nokogiri::XML(last_response.body)
+    expected_xml = Nokogiri::XML(File.read(File.join(FIXTURES_DIR, 'crowdsourcing_bridget_1.xml')))
+    expect(EquivalentXml.equivalent?(returned_xml, expected_xml, opts = { :ignore_attr_values => 'datetime'})).to be_truthy
+  end
+
+  it "returns normalized XML" do
+    post "/v1/modsulator", "file" => Rack::Test::UploadedFile.new(File.join(FIXTURES_DIR, 'crowdsourcing_bridget_1.xlsx'), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    returned_xml = Nokogiri::XML(last_response.body)
+    expected_xml = Nokogiri::XML(File.read(File.join(FIXTURES_DIR, 'crowdsourcing_bridget_1.xml')))
+    expect(EquivalentXml.equivalent?(returned_xml, expected_xml, opts = { :ignore_attr_values => 'datetime'})).to be_truthy
   end
 end
