32e7ac1642995e067c1ac73810cc6c2d711e5dfd
Tommy Ingulfsen <tommyi@stanford.edu>
2016-05-03 22:52:55 -0700

    even more debugging of file parameters

--- lib/modsulator_app.rb
+++ lib/modsulator_app.rb
@@ -32,6 +32,7 @@ module Spreadsheet
       post do
         LOG.error("Tommy: here")
         LOG.error("Tommy: params = #{params}")
+        LOG.error("Tommy: inspect #{params[:file].inspect}")
 #        LOG.error("Tommy: received fileparams = #{params[:file]} and tempfile = #{params[:file][:tempfile]} and filename = #{params[:filename]}")
         mods_converter = Modsulator.new(File.new(params[:file][:tempfile]), params[:filename])
         puts("Tommy: mods_converter = #{mods_converter}")
